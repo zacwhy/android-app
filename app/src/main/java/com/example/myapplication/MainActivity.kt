0f238647ca3e10d2142d61fb31b73d0e62057a8b
ZY <zacwhy@gmail.com>
2019-10-30 13:50:17 +0800

    Add Floating Action Button
    Floating Action Button in Kotlin
    https://www.youtube.com/watch?v=RgAveUJvxbY

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab_btn.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
